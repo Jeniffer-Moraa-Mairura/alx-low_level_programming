1b93615b37733d0f9ee67abc7e4d2c4a6646304d
Jeniffer-Moraa-Mairura <mjenna303@gmail.com>
2022-10-19 12:25:37 -0700

    nested 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,10 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 	{
-		for (letter = 'a'; letter <= 'z'; k++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar (letter);
 		}
 		_putchar('\n');
 	}
+}
