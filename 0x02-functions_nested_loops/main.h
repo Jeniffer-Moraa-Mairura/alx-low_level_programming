87426509fb52c54310c71978b555c7e262ba8ed1
Jeniffer-Moraa-Mairura <mjenna303@gmail.com>
2022-10-19 03:25:24 -0700

    nested functions

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 int _islower(int c);
 void print_alphabet_x10(void);
-void print_alphabet(void);
+void print_alphabet(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
