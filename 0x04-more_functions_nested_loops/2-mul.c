d403cdfd31d4ac5ef0d6624532cf2dbd82732f9d
Jeniffer-Moraa-Mairura <mjenna303@gmail.com>
2022-10-21 15:10:54 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  *mul - multiplies two integers
+  *@a:first parameter
+  *@b:second parameter
+  *Return: addition of multiplication
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,14 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <unistd.h>
 #include <stdio.h>
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
 void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_numbers(void);
+int mul(int a, int b);
+int _isdigit(int c);
+int _isupper(int c);
 void print_triangle(int size);
 #endif
